37505744cec4eb868ee0a07b4c1953289373c607
SergiyKochenko <sergiykochenko@gmail.com>
2023-02-28 10:57:14 +0000

    first deploymenr on heroku

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://cesebnkq:rZ6IDU3DNADCV6bY08f0bG7pb3Lj6BMZ@manny.db.elephantsql.com/cesebnkq"
+os.environ["SECRET_KEY"]="w=e$xe2$nz#n!cg5sb#wx(m8(h5te-!b5+s0+t)q0jyk!!#4v+"
+os.environ["CLOUDINARY_URL"] = "cloudinary://788785116973719:8U9Kg-ES3PA756lv4s8J3f7g3Ig@dvbhrs1gf"
